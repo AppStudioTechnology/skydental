c666ea89d72b743449fcf87c4a2475b9a4e03c01
aliaslam90 <aliaslam@Alis-MacBook-Pro-2.local>
2026-03-11 12:43:45 +0400

    general

--- src/app/components/DoctorsSection.tsx
+++ src/app/components/DoctorsSection.tsx
@@ -50,7 +50,7 @@ export default function DoctorsSection() {
   }
 
   return (
-    <section id="doctors" className="py-[80px] px-[16px] md:px-[20px] lg:px-[25px] bg-[#e0edff]">
+    <section id="doctors" className="pt-0 pb-[80px] px-[16px] md:px-[20px] lg:px-[25px] bg-[#e8f5e9]">
       <div className="max-w-[1390px] mx-auto">
         <motion.div
           initial={{ y: 30, opacity: 0 }}
